5f174df7ba076555485ec50d2767c074419ec2cb
AntonZavalko <nikolajzavalko40@gmail.com>
2025-11-28 19:44:36 +0200

    Оновлення

--- src/config/env.js
+++ src/config/env.js
@@ -8,7 +8,5 @@ module.exports = {
   SERVER_ID: process.env.SERVER_ID || os.hostname(),
   CACHE_ENABLED: process.env.CACHE_ENABLED !== 'false',
   DATABASE_URL: process.env.DATABASE_URL,
-
-  // НОВЕ — домен для коротких посилань
   BASE_URL: process.env.BASE_URL || `http://localhost:${process.env.PORT || 3000}`
 };
